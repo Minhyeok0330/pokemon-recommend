a5c0dbad510647e153e19eee15ff10a4a60a35de
Minhyeok0330 <alsgurgud@naver.com>
2025-03-17 18:58:19 +0900

    image done

--- pokemon_app/views.py
+++ pokemon_app/views.py
@@ -6,13 +6,15 @@ from bs4 import BeautifulSoup
 # Create your views here.
 def poke_members(request, username):
     poke_num = [str(i).zfill(4) for i in range(1, 1026)]
-    my_pokemon_numbers = random.choice(poke_num)
+    my_pokemon_numbers = random.sample(poke_num, 6)
+    for my_pokemon_num in my_pokemon_numbers:
+        my_pokemon_num = my_pokemon_num
 
     context = {
         'username': username,
-        'my_pokemon_numbers': f'https://data1.pokemonkorea.co.kr/newdata/pokedex/full/{my_pokemon_numbers}01.png'
-,
+        'my_pokemon_numbers': my_pokemon_numbers
     }
+       
 
     # Django 템플릿에 데이터 전달
     return render(request, 'poke_members.html', context)
